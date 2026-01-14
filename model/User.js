7bc0f1277a0a54c67eadf54a3a223b6bddf1dc3d
Risingcoderstar <94625582+Risingcoderstar@users.noreply.github.com>
2026-01-14 15:48:04 +0530

    Update User.js

--- model/User.js
+++ model/User.js
@@ -25,16 +25,7 @@ password:{
 }
 
 })
-userSign.pre("save", async function (next) {
-    if (!this.isModified("password")) return next(); // skip if password isn't changed
-  
-    try {
-      const salt = await bcrypt.genSalt(10);
-      this.password = await bcrypt.hash(this.password, salt);
-      return next();
-    } catch (err) {
-      return next(err);
-    }
-  });
+
 const User=mongoose.model("User",userSign);
+
 module.exports=User;
